b2c47a19749c18eab2a140fa58aa05e5741c34a1
Rajan <139620395+Rajankannaujiya@users.noreply.github.com>
2026-05-12 11:08:48 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://ethara-project-2br3.vercel.app",
     credentials: true,
   })
 );
